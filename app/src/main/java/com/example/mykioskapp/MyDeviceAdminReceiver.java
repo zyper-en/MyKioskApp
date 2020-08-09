56c71ca6180f2064979302169af828a409fbc452
zyper-en <65608563+zyper-en@users.noreply.github.com>
2020-08-09 13:45:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mykioskapp/MyDeviceAdminReceiver.java
@@ -0,0 +1,13 @@
+package com.example.mykioskapp;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+//no implementation needed
+public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
+
+}//MyDeviceAdminReceiver
